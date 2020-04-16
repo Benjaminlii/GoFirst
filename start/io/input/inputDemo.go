3523e4477d52d1fd61d9c153011c009b67b11b4c
Benjamin <litao.benjamin@bytedance.com>
2020-04-16 17:17:25 +0800

    键盘输入

---
+++ start/io/input/inputDemo.go
@@ -0,0 +1,17 @@
+// package main
+package main
+
+import "fmt"
+
+// 测试键盘输入
+// Go语言中的键盘输入输出与C语言十分相似，区别在于使用的是fmt包中的函数
+
+func main() {
+	var num int
+	//fmt.Scan(&num)
+	fmt.Println(num)
+
+	var str string
+	//fmt.Scan(&str)
+	fmt.Println(str)
+}
