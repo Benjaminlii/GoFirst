bebc1613c42b9134d5b70fcef70518cde9c2bf62
Benjamin <litao.benjamin@bytedance.com>
2020-04-17 22:39:20 +0800

    测试Go函数

--- start/function/function.go
+++ start/function/function.go
@@ -54,6 +54,34 @@ func init() {
 	fmt.Println("init")
 }
 
+// 函数的闭包
+// 累加器
+// 这里返回的是一个函数，其具体的定义是内部的匿名函数
+// 但在这个匿名函数内引用了外部的变量count，就形成了闭包
+// 也就是说，匿名函数可以引用其外部的变量，并和这些外部变量共同组成一个闭包
+// 可以理解为整个闭包是一个对象，这个函数是其中的方法，变量则担任成员变量的指责
+// 这里的addCount函数只进行了一次，所以count变量只会被初始化一次，后边被内部的方法修改
+// 生成新的方法也不会影响原先的方法的闭包
+func addCount() func(int) int {
+	var count int = 0
+	return func(num int) int {
+		count += num
+		return count
+	}
+}
+
+// defer机制
+// 在函数中前面标有defer关键字的语句会被压入defer栈
+// 在函数执行完之后，会从defer栈中一条一条的取出语句来执行
+// 因为是栈，所以要注意defer语句执行的顺序
+// 这里使用栈的原因，个人觉得是考虑到资源的释放需要从下向上进行释放
+// 需要注意的是，defer语句中的变量，是一个快照，不会受后面语句的影响而输出不同的结果
+func deferDemo() int {
+	defer fmt.Println("defer 1")
+	defer fmt.Println("defer 2")
+	fmt.Println("do 1")
+	return 1
+}
 func main() {
 	fmt.Println(getSum(1, 1))
 
@@ -71,5 +99,27 @@ func main() {
 	fmt.Println(getSumFromNums(1, 2, 3, 4, 5))
 
 	// 匿名函数
+	// 个人认为匿名函数一般在函数传参处会比较灵活，可以类比Java中的静态内部类传入方法
+	// 求两个数的和的匿名函数
+	num := func(num1 int, num2 int) int {
+		return num1 + num2
+	}(1, 2)
+	fmt.Println(num)
 
+	// 在函数内通过匿名函数定义一个函数，这样就将作用域限制为外层的函数
+	get1 := func() int {
+		return 1
+	}
+	fmt.Println(get1())
+	fun1 := addCount()
+	fmt.Println(fun1(1))
+	fmt.Println(fun1(1))
+	fun2 := addCount()
+	fun2(1)
+	fmt.Println(fun1(1))
+	fmt.Println(fun1(1))
+	fmt.Println(fun1(1))
+
+	// defer
+	fmt.Println(deferDemo())
 }
---
+++ start/timeDemo/timeDemo.go
@@ -0,0 +1,21 @@
+// package main
+package main
+
+// 测试Go中的时间和日期处理包
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	// 测试time包中的时间格式化
+	// 方式一：使用fmt.Sprintf("格式"，参数)来进行格式化填充
+	fmt.Printf("%d-%d-%d %d:%d:%d\n", now.Year(), now.Month(), now.Day(),
+		now.Hour(), now.Minute(), now.Second())
+	// 方式二：使用time.Time对象的Format方法进行格式化
+	// 其中 2006，1，2，15，4，5这些数组都是固定代表某一个时间属性的，类比Java中的YYYY等等
+	timeStr := now.Format("2006/01/02 15:04:05")
+	fmt.Println(timeStr)
+}
