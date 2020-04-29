0ab1897897f4b2f1172d84c1c0dbcc09d2c03715
Benjamin <litao.benjamin@bytedance.com>
2020-04-29 11:51:15 +0800

    指针和对象的用法

--- hello/hello.go
+++ hello/hello.go
@@ -3,6 +3,20 @@ package main
 
 import "fmt"
 
+type a struct {
+	name string
+	age  int
+}
+
+func (a *a) print() {
+	fmt.Printf("a.name = %v, a.age = %v\n", a.name, a.age)
+}
+
 func main() {
-	fmt.Printf("hello, world\n")
+	var aObj a = a{name: "lt", age: 20}
+	bObj := new(a)
+	bObj.name = "lt1"
+	bObj.age = 20
+	aObj.print()
+	bObj.print()
 }
